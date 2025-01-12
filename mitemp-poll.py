b9bbe78a16dfcdab2cbed02cfea76f31c9196344
vitamin-caig <vitamin.caig@gmail.com>
2025-01-12 16:01:49 +0300

    Fetch notifications via separate gatt process. Fix debounce.

--- mitemp-poll.py
+++ mitemp-poll.py
@@ -6,11 +6,13 @@ import time
 from datetime import datetime, timedelta
 import logging
 import os
+from random import randint
 import re
 import struct
-import sys
 from subprocess import call
-from multiprocessing import Process
+import sys
+import threading
+from threading import Thread, Lock
 
 class Event(object):
     def __init__(self, mac, key, value):
@@ -24,11 +26,10 @@ class Event(object):
 class Controller(object):
     EVENT = re.compile(r'Device (?P<mac>..:..:..:..:..:..) (?P<key>[^:]+): ?(?P<value>.+)?')
     ANNOUNCE = re.compile(r'Device (?P<mac>..:..:..:..:..:..) (?P<alias>.+)')
-    ATTR = re.compile(r'/org/bluez/hci\d+/dev_(?P<mac>.._.._.._.._.._..)/(?P<key>\S+) Notification:')
     DUMP = re.compile(r' (?P<bin>( [0-9a-f]{2}){1,16})\s+')
 
-    def __init__(self, timeout):
-      self._pipe = pexpect.spawn('bluetoothctl', encoding='utf-8', timeout=timeout.total_seconds())
+    def __init__(self):
+        self._pipe = pexpect.spawn('bluetoothctl', encoding='utf-8', timeout=300)
 
     def scan(self):
         logging.debug('Scan started')
@@ -40,17 +41,11 @@ class Controller(object):
         lastevent = None
         while True:
             line = self._pipe.readline().strip()
-            #print(line, file=sys.stderr)
             if evt := Controller.EVENT.search(line):
                 data = evt.groupdict()
                 if lastevent:
                     yield lastevent
                 lastevent = Event(mac=data['mac'], key=data['key'], value=data['value'])
-            elif attr := Controller.ATTR.search(line):
-                data = attr.groupdict()
-                if lastevent:
-                    yield lastevent
-                lastevent = Event(mac=data['mac'].replace('_', ':'), key=data['key'], value=None)
             elif ann := Controller.ANNOUNCE.search(line):
                 data = ann.groupdict()
                 yield Event(mac=data['mac'], key='Name', value=data['alias'])
@@ -66,19 +61,39 @@ class Controller(object):
             else:
                 logging.debug(f'Unexpected line "{line}"')
 
-    def connect(self, mac):
-        logging.debug(f'Try to connect {mac}')
-        self._pipe.sendline(f'connect {mac}')
+def connect_gatt(mac, handle, val):
+    if not hasattr(connect_gatt, 'lock'):
+        connect_gatt.lock = Lock()
+    with connect_gatt.lock:
+        pipe = pexpect.spawn(f'gatttool -b {mac} --char-write-req --handle={handle} --value={val} --listen', encoding='utf-8')
+        idx = pipe.expect([pexpect.EOF, pexpect.TIMEOUT, r'written successfully'], timeout=randint(30, 60))
+        if idx < 2:
+            pipe.terminate(force=True)
+            return None
+        return pipe
 
-    def disconnect(self):
-        logging.debug('Disconnect current device')
-        self._pipe.sendline('gatt.release-notify')
-        self._pipe.sendline('disconnect')
-
-    def select_notify(self, attr):
-        logging.debug(f'Enable notifications from {attr}')
-        self._pipe.sendline(f'gatt.select-attribute {attr}')
-        self._pipe.sendline('gatt.acquire-notify')
+def listen_notifications(mac, handle, val):
+    if not hasattr(listen_notifications, 'lock'):
+        listen_notifications.lock = Lock()
+    logger = logging.getLogger(mac)
+    while True:
+        logger.debug('Connect')
+        pipe = connect_gatt(mac, handle, val)
+        if not pipe:
+            continue
+        logger.debug('Listen events')
+        while True:
+            idx = pipe.expect([pexpect.EOF, pexpect.TIMEOUT,
+                               r'error\r\n',
+                               r'Notification handle = (?P<handle>0x[0-9a-f]+) value:(?P<bin>( [0-9a-f]{2})+)\s*\r\n'], timeout=randint(30, 60))
+            if idx < 3:
+                logger.debug('Restart')
+                pipe.terminate(force=True)
+                break
+            match = pipe.match.groupdict()
+            logger.debug(f"Notified {match['handle']}={match['bin']}")
+            txt = match['bin'].replace(' ', '')
+            yield bytes.fromhex(txt)
 
 def parse_rssi(val):
     return re.search(r'-\d+', val).group(0)
@@ -148,12 +163,16 @@ class XSDevice(object):
     def parse_event(self, key, value):
         if key == 'RSSI':
             return [('rssi', parse_rssi(value))]
-        elif key.startswith('service000'):
-            return XSDevice._parse_attribute(value)
-        return []
+        else:
+            return []
+
+    def scan(self, dst):
+        for payload in listen_notifications(self._mac, '0x000d', '0x2'):
+            for k, v in XSDevice._parse_notification(payload):
+                dst(self._mac, k, v)
 
     @staticmethod
-    def _parse_attribute(payload):
+    def _parse_notification(payload):
         id, event, _ = struct.unpack('<BHB', payload[:4])
         if id != 0x23:
             return []
@@ -171,108 +190,29 @@ class XSDevice(object):
             logging.debug(f'Unexpected xs payload {payload.hex()}')
             return []
 
-    def on_services_resolved(self, ctrl):
-        ctrl.select_notify('0000c761-0000-1000-8000-00805f9b34fb')
-
-class ConnectionStatus(object):
-    MAX_CONNECTED = timedelta(seconds=30)
-    RECONNECT_PERIOD = timedelta(minutes=2)
-
-    def __init__(self):
-        self._last_activity = datetime.fromtimestamp(0)
-        self._devices = dict() # mac => status
-
-    def connect(self, mac):
-        self._devices[mac] = 'Connecting'
-
-    def disconnect(self):
-        for (mac, status) in self._devices.items():
-            if status == 'Connected':
-                self._devices[mac] = 'Disconnecting'
-
-    def on_connected(self, mac):
-        self._devices[mac] = 'Connected'
-        self._update()
-
-    def on_disconnected(self, mac):
-        self._devices.pop(mac, None)
-        self._update()
-
-    def _update(self):
-        self._last_activity = datetime.now()
-
-    def may_connect(self, now):
-        return len(self._devices) == 0 and self._is_expired(ConnectionStatus.RECONNECT_PERIOD, now)
-
-    def need_disconnect(self, now):
-        return 'Connected' in self._devices.values() and self._is_expired(ConnectionStatus.MAX_CONNECTED, now)
-
-    def _is_expired(self, period, now = datetime.now()):
-        return now - self._last_activity > period
-
 class DevicesList(object):
     def __init__(self, ctrl):
         self._ctrl = ctrl
         self._devs = {}
-        self._connectable = []
-        self._status = ConnectionStatus()
-        self._rr_index = -1
 
     def start(self, dst):
         self._ctrl.scan()
         for evt in self._ctrl.listen():
-            for d in self._process_event(evt):
-                dst(*d)
+            self._process_event(evt, dst)
 
-
-    def _process_event(self, evt):
-        self._dispatch_connections()
+    def _process_event(self, evt, dst):
         if evt.mac not in self._devs:
-            self._maybe_add_device(evt)
+            self._maybe_add_device(evt, dst)
         if evt.mac in self._devs:
-            if evt.key == 'Connected':
-                self._process_connection(evt)
-            elif evt.key == 'ServicesResolved':
-                self._process_service_resolve(evt)
-            else:
-                return [(evt.mac, k, v) for k, v in self._devs[evt.mac].parse_event(evt.key, evt.value)]
-        return []
+            for k, v in self._devs[evt.mac].parse_event(evt.key, evt.value):
+                dst(evt.mac, k, v)
 
-    def _dispatch_connections(self):
-        now = datetime.now()
-        if self._status.need_disconnect(now):
-            self._status.disconnect()
-            self._ctrl.disconnect()
-        elif self._status.may_connect(now):
-            self._connect_next()
-
-    def _maybe_add_device(self, evt):
+    def _maybe_add_device(self, evt, dst):
         if mijia := MijiaDevice.maybe_create(evt):
             self._devs[evt.mac] = mijia
         elif xs := XSDevice.maybe_create(evt):
             self._devs[evt.mac] = xs
-            self._connectable.append(xs)
-
-    def _process_connection(self, evt):
-        logging.debug(f'Connected {evt.mac}: {evt.value}')
-        if evt.value == 'yes':
-            self._status.on_connected(evt.mac)
-        else:
-            self._status.on_disconnected(evt.mac)
-
-    def _process_service_resolve(self, evt):
-        logging.debug(f'Resolved {evt.mac}: {evt.value}')
-        if evt.value == 'yes':
-            self._devs[evt.mac].on_services_resolved(self._ctrl)
-
-    def _connect_next(self):
-        candidates = len(self._connectable)
-        if candidates == 0:
-            return
-        self._rr_index = (self._rr_index + 1) % candidates
-        dev = self._connectable[self._rr_index]
-        self._status.connect(dev._mac)
-        self._ctrl.connect(dev._mac)
+            Thread(name=f'Scanner for {evt.mac}', target=lambda: xs.scan(dst)).start()
 
 class Handler(object):
     def __init__(self, handler):
@@ -296,24 +236,25 @@ class Debouncer(object):
     def __init__(self, delegate, timeout):
         self._delegate = delegate
         self._timeout = timeout
-        self._filt = dict()
+        self._buffer = dict()
+        self._lock = Lock()
 
     def __call__(self, mac, type, value):
-        now = datetime.now()
-        key = (mac, type)
-        (prev, ts) = self._filt.get(key, (None, None))
-        if prev == value and now - ts < self._timeout:
-            logging.debug(f'Drop {mac}: {type}={value}')
-        else:
-            self._filt[key] = (value, now)
-            self._delegate(mac, type, value)
+        with self._lock:
+            now = datetime.now()
+            if len(self._buffer) == 0:
+                self._next_flush = now + self._timeout
+            self._buffer[(mac, type)] = value
+            if now > self._next_flush:
+                for (m, t), v in self._buffer.items():
+                    self._delegate(m, t, v)
+                self._buffer.clear()
 
 def main():
     parser = ArgumentParser(description='Mijia scanning daemon')
     parser.add_argument('--verbose', action='store_true', help='Be verbose')
     parser.add_argument('--handler', help='Call specified binary with {{mac}},{{type}},{{value}} placeholders')
     parser.add_argument('--format', help='Format string with {{mac}},{{type}} and {{value}} placeholders and flush to stdout')
-    parser.add_argument('--timeout', default=5, type=int, help='Timeout for no data in minutes')
     parser.add_argument('--debounce', default=0, type=int, help='Enable debounce of duplicated values for specified amount of seconds')
 
     args = parser.parse_args()
@@ -330,7 +271,7 @@ def main():
     if args.debounce:
         dst = Debouncer(dst, timedelta(seconds=args.debounce))
 
-    ctrl = Controller(timedelta(minutes=args.timeout))
+    ctrl = Controller()
     devs = DevicesList(ctrl)
     devs.start(dst)
 
